c1d5423554805adf66dc5de3b5207b482da3d498
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-03 22:59:21 -0800

    added first view, created urls.py

--- slugbites/main/views.py
+++ slugbites/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(response) -> HttpResponse:
+    return HttpResponse("<h1>Hello world!<h1>")
