5f4cec54a3307dd602592cc30b6a69ee2faf783f
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-04 00:53:08 -0800

    Renamed v1 to food

--- slugbites/main/views.py
+++ slugbites/main/views.py
@@ -7,4 +7,5 @@ def index(response) -> HttpResponse:
     return HttpResponse("<h1>Hello world!<h1>")
 
 def food(response) -> HttpResponse:
-    return HttpResponse("<h1>Success!<h1>")
+    ls = foodItem.all() # This should be a list of all food items int the database
+    return HttpResponse("<h1>Food:<h1>" %(ls.name)) # This should display said items
