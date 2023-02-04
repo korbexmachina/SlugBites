94b35becbeaa09e4db222f9b67c33364f8ec4705
Cheuk Pui Lam <cheuk@merrill-100-64-109-67.resnet.ucsc.edu>
2023-02-04 01:47:53 -0800

    added parsing logic and functionality

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 #! usr/bin/env/python3
 """
-TODO: Docstring to describe what this file does 
+TODO: Docstring to describe what this file does
 """
 __author__ = "Korben Tompkin, Kayla Barker and Cheuk Pui Lam"
 __licence__ = "MPL 2.0"
@@ -10,6 +10,7 @@ import asyncio
 import json
 import aiohttp
 import datetime
+from dataclasses import dataclass
 
 from typing import Dict
 
@@ -41,3 +42,34 @@ class Meal:
     tags: List[str]
     location: str
     time: str  # options are Breakfast,Lunch, Dinner,Late Night
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class AllData:
+    def __init__(self, data: List[Dict]) -> None:
+        self.data = self.parse_data(data)
+
+    def parse_data(self, data: List[Dict]) -> List[Meal]:
+        """Parsing data from the api and outputs it as a list of meals
+
+        Args:
+            data (List[Dict]): datas
+
+        Returns:
+            List[Meal]: list of meals within the 7 day period
+        """
+        r = []
+        for days in data:
+            curr_date = datetime.datetime.strptime(
+                days['date'], "%Y-%m-%dT%H:%M:%S").date()
+            for halls in days['halls']:
+                curr_hall = halls['name']
+                for m in halls['meals']:
+                    curr_time = m['meal']
+                    for c in m['cats']:
+                        for foods in c['foods']:
+                            r.append(Meal(food['name'], curr_date, list(
+                                foods['legend'].keys()), curr_hall, curr_time))
+        return r
