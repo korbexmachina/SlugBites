80e9446764afca5a4636bfb7992ec7f069b73a18
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-03 23:59:36 -0800

    implemented foodItems class for database

--- slugbites/main/models.py
+++ slugbites/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class foodItems(models.Model):
+    name = models.CharField(max_length=200)
+    veggie = models.BooleanField()
+    soy = models.BooleanField()
+    eggs = models.BooleanField()
+    milk = models.BooleanField()
+    vegan = models.BooleanField()
+    gluten = models.BooleanField()
+    halal = models.BooleanField()
+    unknown = models.BooleanField()
+    time = models.BooleanField()
+    
+    def __str__(self):
+        return self.name()
