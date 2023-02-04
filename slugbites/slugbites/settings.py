9655708c59e5b837ccc9c098754006b371c33bac
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-04 15:43:59 -0800

    environment things

--- slugbites/slugbites/settings.py
+++ slugbites/slugbites/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_94lg)+_1(rmd1te2g0_el)d@e-d0a9^ujka+yva082i7v&wtg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
