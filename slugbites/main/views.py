34498528dd5029aabbaa14f56ef856fefb131e3f
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-04 01:30:32 -0800

    moved food to the new style

--- slugbites/main/views.py
+++ slugbites/main/views.py
@@ -6,7 +6,6 @@ from .models import foodItem
 def index(response) -> HttpResponse:
     return render(response, "main/base.html", {})
 
-# TODO: fix this
 def food(response) -> HttpResponse:
     ls = str(foodItem) # This should be a list of all food items int the database
-    return HttpResponse("<h1>Food:</h1><br></br><p>%s</p>" %(ls)) # This should display said items
+    return render(response, "main/food.html" %(ls), {}) # This should display said items
