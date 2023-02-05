e294f9d0ac7d75d20f9ddf32260d9c0279b9e622
Cheuk Pui Lam <lcpmatthew@gmail.com>
2023-02-05 06:45:49 -0800

    rewrote files, removed unused files

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+""" 
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
+    path("food/", views.food, name="food"),
+    path("currentmeal/", views.currentmeal, name="currentmeal"),
+    path("results/", views.search, name="search")
+]
