73e81b5d384ceef1594b7aeb28785fea1b53287d
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-04 16:54:09 -0800

    moved frontend directories out of slugbites

--- static/index-bundle.js
+++ static/index-bundle.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/index.js":
+/*!*************************!*\
+  !*** ./assets/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("function component() {\n    const element = document.createElement('div');\n    element.innerHTML = 'Hello webpack';\n    return element;\n}\ndocument.body.appendChild(component());\n\n\n//# sourceURL=webpack://trash-pandas/./assets/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./assets/index.js"]();
 /******/ 	
 /******/ })()
 ;
