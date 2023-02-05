c608092ac732371616e469819e4cd1c07f18c43f
Cheuk Pui Lam <lcpmatthew@gmail.com>
2023-02-04 18:17:37 -0800

    pushing for korben

--- slugbites/slugbites/settings.py
+++ slugbites/slugbites/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
-    'slugbites'
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
