43a0af2ad8c76b34c41c42d55916e11f09988984
Cheuk Pui Lam <lcpmatthew@gmail.com>
2023-02-04 16:09:22 -0800

    added virtual environment script

---
+++ venvsetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# check if python3 exists
+if ! [[ $(python3 --version) =~ ^Python\ 3* ]]
+then
+    exit;
+fi
+# set up current working directory
+PWD=$(pwd)
+
+# check if virtual environment exists
+if ! [[ -d "venv" ]]
+then
+    # if virtual environment doesn't exist, build virtual environment
+    python3 -m venv venv;
+fi
+
+# give execute permision to activate file
+chmod +x $PWD/venv/bin/activate;
+
+# runs the activate script
+activate;
+
+# upgrades pip
+pip install --upgrade pip;
+
+# installs requirements
+pip install -r requirements.txt;
