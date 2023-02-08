d71338d6363ce7fe46ce826279eb5c30eda23814
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-07 22:17:21 -0800

    changing STATIC_ROOT

--- slugbites/settings.py
+++ slugbites/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
