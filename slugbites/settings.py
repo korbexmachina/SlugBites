9a762bc00dc45e7780f953e248c6c127b672b370
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-13 21:22:55 -0800

    entered debug mode

--- slugbites/settings.py
+++ slugbites/settings.py
@@ -30,7 +30,7 @@ TIME_ZONE = "America/Los_Angeles"
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "slugbites.azurewebsites.net",
