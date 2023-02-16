2b5b898f80b5815a7167b2b85034cb21983cf149
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-02-16 00:56:41 -0800

    fixed last commit

--- slugbites/settings.py
+++ slugbites/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'slugbites_api.apps.Slugbites_apiConfig',
+    'slugbites_api.apps.SlugbitesApiConfig',
     'pwa'
 ]
 
